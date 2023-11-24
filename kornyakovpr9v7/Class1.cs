8dca50bd1ef2042807d8a69fd7770e56b5fc6507
ИСП-31 <kolawhiskey.edits@gmail.com>
2023-11-24 14:56:42 +0300

    Добавьте файлы проекта.

---
+++ kornyakovpr9v7/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kornyakovpr9v7
+{
+    public struct PersonData
+    {
+
+        private string _name, _familya, _otchestvo, _address;
+        private ulong _phoneNumber;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Familya
+        {
+            get { return _familya; }
+            set { _familya = value; }
+        }
+        public string Otchestvo
+        {
+            get { return _otchestvo; }
+            set { _otchestvo = value; }
+        }
+        public ulong PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        public PersonData()
+        {
+            _name = "Введите имя";
+            _familya = "Введите фамилию";
+            _otchestvo = "Введите отчество";
+            _address = "Введите адрес";
+            _phoneNumber = 0;
+        }
+
+        public static List<string> GetResult(List<PersonData> list, string address)
+        {
+            List<string> result = new List<string>();
+
+            foreach (PersonData i in list)
+            {
+                if (i.Address == address) result.Add(i.Name);
+            }
+            if (result.Count == 0)
+            {
+                throw new ArgumentException("Таких  нет у нас на районе");
+            }
+            return result;
+        }
+
+    }
+}
---
+++ kornyakovpr9v7/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kornyakovpr9v7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<PersonData> list = new List<PersonData>();
+        PersonData p1 = new(),
+            p2 = new(),
+            p3 = new(),
+            p4 = new(),
+            p5 = new(),
+            p6 = new(),
+            p7 = new();
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа№9 Заполнить таблицу со списком телефонных абонентов на 7 человек с полями: ФИО, \r\nномер телефона, адрес. Вывести результат на экран. Вывести список абонентов \r\nживущих на заданной улице.  Разработчик Корняков Д.Д.");
+        }
+
+        private void dataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+            list.Add(p4);
+            list.Add(p5);
+            list.Add(p6);
+            list.Add(p7);
+
+            dataGrid.ItemsSource = list;
+        }
+
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                tbResult.Clear();
+
+                string adress = tbClassNumber.Text;
+
+                List<string> result = PersonData.GetResult(list, adress);
+
+                foreach (string name in result)
+                {
+                    tbResult.Text = tbResult.Text + name + "\n";
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+                tbResult.Clear();
+                tbClassNumber.Clear();
+            }
+
+        }
+
+        private void dataGrid_CellEditing(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            int columnIndex = e.Column.DisplayIndex,
+                rowIndex = e.Row.GetIndex();
+
+            try
+            {
+                PersonData temp = list[rowIndex];
+
+                switch (columnIndex)
+                {
+                    case 0:
+                        temp.Name = ((TextBox)e.EditingElement).Text;
+                        break;
+                    case 1:
+                        temp.Familya = ((TextBox)e.EditingElement).Text;
+                        break;
+                    case 2:
+                        temp.Otchestvo = ((TextBox)e.EditingElement).Text;
+                        break;
+                    case 3:
+                        temp.PhoneNumber = Convert.ToUInt64(((TextBox)e.EditingElement).Text);
+                        break;
+                    case 4:
+                        temp.Address = ((TextBox)e.EditingElement).Text;
+                        break;
+                }
+                list[rowIndex] = temp;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректный ввод");
+                ((TextBox)e.EditingElement).Text = "0";
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Некорректный ввод");
+                ((TextBox)e.EditingElement).Text = "0";
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+
+       
+        }
+    }
+}
